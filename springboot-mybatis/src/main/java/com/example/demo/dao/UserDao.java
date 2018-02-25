2114d0677a4c87cee3c0d3d26abdf27651ba5a12
youyang87111 <youyang_java@139.com>
2018-02-25 22:33:31 +0800

    xx

---
+++ springboot-mybatis/src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+
+import java.util.List;
+import com.example.demo.domain.User;
+
+public interface UserDao {
+
+    List<User> list();
+}
---
+++ springboot-mybatis/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.domain.User;
+
+public interface UserService {
+
+	List<User> list();
+}
